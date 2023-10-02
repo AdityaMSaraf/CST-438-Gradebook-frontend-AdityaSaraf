8e1fa5fb2a88827962a3b8c4681116ba145e80eb
Aditya Saraf <79900612+AdityaMSaraf@users.noreply.github.com>
2023-10-01 20:49:46 -0700

    Added add, delete and update functionality to frontend

--- src/components/EditAssignment.js
+++ src/components/EditAssignment.js
@@ -1,13 +1,106 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {Dialog, DialogActions, DialogContent, Button, TextField} from '@mui/material';
+import {SERVER_URL} from "../constants";
 
 
-function EditAssignment(props) { 
+function EditAssignment({assignment, onClose, reload}) {
+
+    const [editedAssignment, setEditedAssignment] = useState({
+        courseId: assignment ? assignment.courseId : '',
+        assignmentName: assignment ? assignment.assignmentName : '',
+        dueDate: assignment ? assignment.dueDate : '',
+    });
+
+    const [message, setMessage] = useState('');
+    const [editDialog, setEditDialog] = useState(true);
 
 
-  return (
-      <div>
-      </div>
-  ); 
+    useEffect(() => {
+
+    }, [])
+
+    const handleEdit = (e) => {
+        const {name, value} = e.target;
+        setEditedAssignment({
+            ...editedAssignment,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = () => {
+        setMessage('');
+        console.log("Assignment.save");
+        fetch(`${SERVER_URL}/assignment/${assignment.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(editedAssignment),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    setMessage('Assignment update successful');
+                    onClose();
+                    setEditDialog(false);
+                    reload();
+                } else {
+                    setMessage("Assignment update failed");
+                }
+            })
+            .catch((error) => {
+                console.error("There was an error: " + error);
+                setMessage("Error updating assignment");
+            });
+    };
+
+    return (
+        <div>
+            <Dialog open={editDialog} onClose={onClose}>
+                <DialogContent>
+                    <TextField
+                        label="Assignment Name"
+                        variant="outlined"
+                        fullWidth
+                        name="assignmentName"
+                        value={editedAssignment.assignmentName}
+                        style={{
+                            paddingBottom: '10px'
+                        }}
+                        onChange={handleEdit}>
+                    </TextField>
+                    <TextField
+                        label="Course ID"
+                        name="courseId"
+                        variant="outlined"
+                        value={editedAssignment.courseId}
+                        onChange={handleEdit}
+                        style={{
+                            paddingBottom: '10px'
+                        }}
+                        fullWidth>
+                    </TextField>
+                    <TextField
+                        type="date"
+                        label="Due Date"
+                        name="dueDate"
+                        value={editedAssignment.dueDate}
+                        fullWidth
+                        onChange={handleEdit}
+                        variant="outlined">
+                    </TextField>
+                    <p>{message}</p>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleSubmit} color="primary">
+                        Update
+                    </Button>
+                    <Button onClick={onClose} color="primary">
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
 
 export default EditAssignment;
--- src/components/ListAssignment.js
+++ src/components/ListAssignment.js
@@ -1,61 +1,117 @@
 import React, {useState, useEffect} from 'react';
 import {SERVER_URL} from '../constants';
 import {Link} from 'react-router-dom';
+import {Button} from '@mui/material';
+import EditAssignment from "./EditAssignment";
+import AddAssignment from "./AddAssignment";
 
 
 function ListAssignment(props) {
 
-  const [assignments, setAssignments] = useState([]);
-  const [message, setMessage] = useState('');
+    const [assignments, setAssignments] = useState([]);
+    const [message, setMessage] = useState('');
+    const [editDialogOpen, setEditDialogOpen] = useState(false);
+    const [addDialogOpen, setAddDialogOpen] = useState(false);
+    const [selectedAssignment, setSelectedAssignment] = useState(null);
 
-  useEffect(() => {
-   // called once after intial render
-   fetchAssignments();
-  }, [] )
- 
-  const fetchAssignments = () => {
-    console.log("fetchAssignments");
-    fetch(`${SERVER_URL}/assignment`)
-    .then((response) => response.json() ) 
-    .then((data) => { 
-      console.log("assignment length "+data.length);
-      setAssignments(data);
-     }) 
-    .catch(err => console.error(err)); 
-  }
-  
-  
-    const headers = ['Assignment Name', 'Course Title', 'Due Date', ' ', ' ', ' '];
-    
+
+    useEffect(() => {
+        // called once after intial render
+        fetchAssignments();
+    }, [])
+
+    const fetchAssignments = () => {
+        console.log("fetchAssignments");
+        fetch(`${SERVER_URL}/assignment`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("assignment length " + data.length);
+                setAssignments(data);
+            })
+            .catch(err => console.error(err));
+    }
+
+    const handleDelete = (assignment, force) => {
+        fetch(`${SERVER_URL}/assignment/${assignment.id}?force=${force}`, {
+            method: 'DELETE'
+        })
+            .then((response) => {
+                if(response.ok){
+                    setMessage('Assignment deleted');
+                    fetchAssignments();
+                }else if(response.status === 400){
+                    setMessage('Assignment has grades, use Force Delete');
+                }else{
+                    setMessage('Error deleting assignment');
+                }
+            })
+            .catch((error) => {
+                console.error('Error deleting assignment: ' + error);
+                setMessage('Error deleting assignment');
+            })
+    }
+    const openEditDialog = (assignment) => {
+        setSelectedAssignment(assignment);
+        setEditDialogOpen(true);
+    }
+
+    const closeEditDialog = () => {
+        setSelectedAssignment(null);
+        setEditDialogOpen(false);
+    }
+
+    const openAddDialog = () => {
+        setAddDialogOpen(true);
+    }
+
+    const closeAddDialog = () => {
+        setAddDialogOpen(false);
+    }
+
+
+    const headers = ['Assignment Name', 'Course Title', 'Due Date', ' ', ' ', ' ', ' '];
+
     return (
-      <div>
-        <h3>Assignments</h3>
-        <div margin="auto" >
-          <h4>{message}&nbsp;</h4>
-              <table className="Center"> 
-                <thead>
-                  <tr>
-                    {headers.map((title, idx) => (<th key={idx}>{title}</th>))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {assignments.map((row, idx) => (
-                    <tr key={idx}>
-                      <td>{row.assignmentName}</td>
-                      <td>{row.courseTitle}</td>
-                      <td>{row.dueDate}</td>
-                      <td>
-                        <Link to={`/gradeAssignment/${assignments[idx].id}`} >Grade</Link>
-                      </td>
-                      <td>Edit</td>
-                      <td>Delete</td>
+        <div>
+            <h3>Assignments</h3>
+            <div margin="auto">
+                <h4>{message}&nbsp;</h4>
+                <table className="Center">
+                    <thead>
+                    <tr>
+                        {headers.map((title, idx) => (<th key={idx}>{title}</th>))}
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-          </div>
-      </div>
+                    </thead>
+                    <tbody>
+                    {assignments.map((row, idx) => (
+                        <tr key={idx}>
+                            <td>{row.assignmentName}</td>
+                            <td>{row.courseTitle}</td>
+                            <td>{row.dueDate}</td>
+                            <td>
+                                <Link to={`/gradeAssignment/${assignments[idx].id}`}>Grade</Link>
+                            </td>
+                            <td><Button variant='outlined' onClick={() => openEditDialog(row)}>Edit</Button></td>
+                            <td><Button variant='outlined' onClick={()=> handleDelete(row, false)}>Delete</Button></td>
+                            <td><Button variant='outlined' onClick={()=> handleDelete(row, true)}>Force Delete</Button></td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                <Button variant='outlined' onClick={() => openAddDialog()}>Add</Button>
+                <p>&nbsp;</p>
+            </div>
+
+            {editDialogOpen && (
+                <EditAssignment assignment={selectedAssignment} onClose={closeEditDialog} reload={fetchAssignments}/>
+            )}
+
+            {addDialogOpen && (
+                <AddAssignment reload={fetchAssignments} onClose={closeAddDialog}/>
+            )}
+
+        </div>
     )
-}  
+}
 
 export default ListAssignment;
